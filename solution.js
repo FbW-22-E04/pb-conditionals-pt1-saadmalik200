17b139da47fde62833c99bdb8c069bbbf2a878c8
Saad Malik <saad_malik200@hotmail.com>
2022-07-11 12:59:02 +0200

    q1

---
+++ solution.js
@@ -0,0 +1,26 @@
+const int1 = 55;
+const int2 = 80;
+const int3 = 50;
+
+if (
+  (int1 >= 50 && int1 <= 99) ||
+  (int2 >= 50 && int2 <= 99) ||
+  (int3 >= 50 && int3 <= 99)
+) {
+  console.log(true);
+} else {
+  console.log(false);
+}
+
+// if (int1 >= 50 && int1 <= 99) {
+//   console.log(true);
+// } else if (int2 >= 50 && int2 <= 99) {
+//   console.log(true);
+// } else if (int3 >= 50 && int3 <= 99) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+const a = 1,
+  b = 2,
+  c = 3;
